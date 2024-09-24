2985bc04ce96bd34e6929df5b4a41a1499ec94c9
Ryan Gillespie <rgillespie@usgs.gov>
2024-09-24 16:43:58 -0500

    Turn on kickstart plugins

--- init.lua
+++ init.lua
@@ -905,6 +905,13 @@ require('lazy').setup({
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
   -- { import = 'custom.plugins' },
+  --
+  -- Other kickstart plugins
+  require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.gitsigns',
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
