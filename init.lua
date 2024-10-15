0c5e75ceea1ef5197f1eb97538680ea38cd562dd
Ryan Gillespie <rgillespie@usgs.gov>
2024-10-15 15:07:30 -0500

    Leave out declaration from list of lsp references

--- init.lua
+++ init.lua
@@ -452,7 +452,11 @@ require('lazy').setup({
           map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
 
           -- Find references for the word under your cursor.
-          map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+          map('gr', function()
+            require('telescope.builtin').lsp_references {
+              include_declaration = false,
+            }
+          end, '[G]oto [R]eferences')
 
           -- Jump to the implementation of the word under your cursor.
           --  Useful when your language has ways of declaring types without an actual implementation.
