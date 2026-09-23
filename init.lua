b4c2801c93ea2cbeaa24cb5db9b0ff01289021ab
Ryan Gillespie <rgillespie@usgs.gov>
2026-09-23 10:38:36 -0500

    Remove tabline

--- init.lua
+++ init.lua
@@ -439,9 +439,6 @@ do
   ---@diagnostic disable-next-line: duplicate-set-field
   statusline.section_location = function() return '%2l:%-2v' end
 
-  -- tabline
-  require('mini.tabline').setup()
-
   -- ... and there is more!
   --  Check out: https://github.com/nvim-mini/mini.nvim
 end
